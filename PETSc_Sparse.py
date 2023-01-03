be31e04aa64c5f2c775f51797bedc22d51632b17
ooyarce <oioyarce@miuandes.cl>
2023-01-03 17:14:52 -0300

    New Files

--- PETSc_Sparse.py
+++ PETSc_Sparse.py
@@ -26,3 +26,30 @@ def llena(n):
 	A.setValue(n-1,n-1, 2)
 	A.assemble()
 	return A
+def MATRIX_MPIB(n,b):
+	A = PETSc.Mat().createAIJ([n, n]) # AIJ represents sparse matrix
+	A.setUp()
+	#rellenar diagonal
+	for i in range(n):
+		A.setValue(i,i,2)
+	#rellenar bandas
+	for val in range(1,b+1):
+		j = val
+		#rellenar hacia derecha
+		for i in range(n-j):
+			A.setValue(i,j,-1)
+			j+=1
+	#rellenar hacia izquierda
+	for val in range(1,b+1):
+		j = 0
+		for i in range(val,n):
+			A.setValue(i,j,-1)
+			j+=1
+	A.assemble()
+	return A
+
+for i in range(9):
+	n = 10
+	b = i
+	a = MATRIX_MPIB(n,b)
+	print(a.getValues(range(n), range(n)))
