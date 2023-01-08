4ada35161ae1f2e9387016748f27c98a59930378
ooyarce <oioyarce@miuandes.cl>
2023-01-08 17:55:14 -0300

    New files

--- Bandwith_Study.py
+++ Bandwith_Study.py
@@ -14,19 +14,18 @@ from matplotlib import pylab
 comm = MPI.COMM_WORLD
 rank = comm.Get_rank()
 
-nnodos = 1
-ncores = 6
+nnodos = 2
+ncores = 16
 Ncorridas = 10
-list1 = [2, 5, 10,12, 15, 20,30, 40, 45, 50, 55,60, 75, 100,125, 160, 200,250, 350, 500,600]#,2000]#,4000,8000,16000,32000,64000,120000]
-names = [f"Sparse_MPI1{nnodos}x{ncores}.txt",f"Sparse_MPI2{nnodos}x{ncores}.txt",f"Sparse_MPI3{nnodos}x{ncores}.txt",f"Dense_MPI{nnodos}x{ncores}.txt"]
+list1 = [2, 5, 10,12, 15, 20,30, 40, 45, 50, 55,60, 75, 100,125, 160, 200,250, 350, 500,600,2000]#,4000#,8000,16000,32000,64000,120000]
+names = [f"Sparse_90_{nnodos}x{ncores}.txt",f"Sparse_95_{nnodos}x{ncores}.txt",f"Sparse_98_{nnodos}x{ncores}.txt",f"Dense_MPI{nnodos}x{ncores}.txt"]
 files = [open(name,"w") for name in names]
 
 print(f"Trabajando con {nnodos} nodos y {ncores} nucleos")
 
-
 #escribo mis archivos de texto con los tiempos promedio para cada tamaño de matriz
 for N in list1:
-    Bandwitch = [int(N/4),int(N/2),int(3*N/4)]
+    Bandwitch = [int(0.9*N),int(0.95*N),int(0.98*N)]
     print (f"N = {N}")
     print (f"Rank = {rank}\n")
     dts = np.zeros((Ncorridas, len(files)))
@@ -35,9 +34,9 @@ for N in list1:
         #método 1
         pos = 0
         for bandwitch in Bandwitch:
-            t1 = perf_counter()
+            
             A = MATRIX_MPIB(N,bandwitch)
-            t2 = perf_counter()
+            
             b = PETSc.Vec().createMPI(N) # creating a vector
             #b.setValues(range(N), range(1,N+1)) # assigning values to the vector
 
@@ -51,17 +50,18 @@ for N in list1:
             #print ('\\n Solving with:', ksp.getType()) # prints the type of solver
             
             # Solve!
+            t1 = perf_counter()
             ksp.solve(b, x) 
-
+            t2 = perf_counter()
             
             dt = t2-t1
             dts[i][pos] = dt
             pos+=1
 
         #método 2
-        t3 = perf_counter()
+        
         A = llena(N)
-        t4 = perf_counter()
+
         b = PETSc.Vec().createMPI(N) # creating a vector
         #b.setValues(range(N), range(1,N+1)) # assigning values to the vector
 
@@ -75,8 +75,9 @@ for N in list1:
         #print ('\\n Solving with:', ksp.getType()) # prints the type of solver
         
         # Solve!
+        t3 = perf_counter()
         ksp.solve(b, x) 
-
+        t4 = perf_counter()
         
         dt2 = t4-t3
         dts[i][pos] = dt2
@@ -92,8 +93,8 @@ if rank == 0:
     [file.close() for file in files]        
 
     #labels y escalados para el grafico
-    x = [10,20,50,100,200,500,1000,2000]#,4000,8000,16000,32000,64000,120000,240000,480000,1000000,1000000*2,1000000*4,1000000*8,1000000*12]
-    xlab = ["10","20","50","100","200","500","1000","2000"]#,'4000','8000','16000','32000','64000','120000','240000',"480000",'1e06','2e06','4e06','8e06','16e06']
+    x = [10,20,50,100,200,500,1000,2000,4000,8000]#,16000,32000,64000,120000,240000,480000,1000000,1000000*2,1000000*4,1000000*8,1000000*12]
+    xlab = ["10","20","50","100","200","500","1000","2000",'4000','8000']#,'16000','32000','64000','120000','240000',"480000",'1e06','2e06','4e06','8e06','16e06']
     y = [0.1e-3,1e-3,1e-2,0.1,1.,10.,60]
     ylab = ["0.1 ms","1 ms","10 ms","0.1 s","1 s","10 s","1 min"] 
 
@@ -118,7 +119,7 @@ if rank == 0:
     plt.xlabel("Tamaño de la matriz")
     plt.ylabel("Tiempo Transcurrido (s)")
     plt.legend([f"Sparse_MPI1{nnodos}x{ncores}.txt",f"Sparse_MPI2{nnodos}x{ncores}.txt",f"Sparse_MPI3{nnodos}x{ncores}.txt",f"Dense_MPI{nnodos}x{ncores}.txt"],loc = 'upper left')
-    plt.title(f"Ensamblado {nnodos}nodos-{ncores}nucleos")
+    plt.title(f"Solving time {nnodos}nodes-{ncores}cores")
     plt.tight_layout()
     plt.show()
-    plt.savefig(f"Assambly{nnodos}x{ncores}")
+    plt.savefig(f"Solving{nnodos}x{ncores}")
