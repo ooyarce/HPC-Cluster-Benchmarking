be53c857642e26ef731fe73abc1901416b372b3c
ooyarce <oioyarce@miuandes.cl>
2023-01-20 13:27:48 -0300

    fix new files

--- Assambly.py
+++ Assambly.py
@@ -1,73 +1,48 @@
 import numpy as np 
-from numpy import zeros,float32
-from scipy import linalg
-from time import perf_counter
-import matplotlib.pyplot as plt
-from laplacianas import matriz_laplaciana_llena
-from PETSc_Sparse import *
 import petsc4py
-import sys
+from time import perf_counter
+from PETSc_Funcs import *
 from mpi4py import MPI
 petsc4py.init(sys.argv)
 from petsc4py import PETSc
-from matplotlib import pylab
 comm = MPI.COMM_WORLD
 rank = comm.Get_rank()
 
-nnodos = 2
-ncores = 2
+Nnodes = 2
+Ncores = 2
 Nruns = 10
+Nbandwidth = 100
 Nlist = [500,1000,2000]
-
-names = [f"Sparse_N500_{nnodos}x{ncores}.txt",f"Sparse_N1000_{nnodos}x{ncores}.txt",f"Sparse_N2000_{nnodos}x{ncores}.txt",f"Dense_{nnodos}x{ncores}.txt"]
+iterator = 0 
+names = NAMES(Nlist,Ncores,Nnodes)
 files = [open(name,"w") for name in names]
-if rank == 0:
-    print(f"Trabajando con {nnodos} nodos y {ncores} nucleos")
 
 for N in Nlist:
-    Bandwitch = list1(N)
-    dts = np.zeros((Nruns, len(files)))
-    
+    Bandwidth = NLIST(N,Nbandwidth)
+    dts,dtd = np.zeros((Nruns, len(Bandwidth))),np.zeros((Nruns, len(Bandwidth)))    
     for i in range(Nruns):
-        #método 1
         pos = 0
-        for bandwitch in Bandwitch:
+        for b in Bandwidth:
             t1 = perf_counter()
-            A = MATRIX_MPIB(N,bandwitch)
+            A = MATRIX_MPIB(N,b)
             t2 = perf_counter()
-            dt = t2-t1
-            dts[i][pos] = dt
-            pos+=1
-        #método 2 
-        t3 = perf_counter()
-        A = llena(N)
-        t4 = perf_counter()
-        dt2 = t4-t3
-        dts[i][pos] = dt2
+            dts[i][pos] = t2-t1
             
-    dts_mean = [np.mean(dts[:,j]) for j in range(len(files))]
+            t3 = perf_counter()
+            A = MATRIX_DENSE(N,b)
+            t4 = perf_counter()
+            dtd[i][pos] = t4-t3
+            
+            pos+=1
 
+    dts_mean,dtd_mean = [np.mean(dts[:,j]) for j in range(len(Bandwidth))],[np.mean(dtd[:,j]) for j in range(len(Bandwidth))]
+    print(f"Trabajando con {Nnodes} nodos y {Ncores} nucleos\nlen = {len(Bandwidth)}, N = {N}\n")
     if rank == 0:
-        for i in range(len(files)):
-            files[i].write(f"{N} {dts_mean[i]}\n")
-            files[i].flush()
-if rank == 0:
-    [file.close() for file in files]        
-    #labels y escalados para el grafico
-    x = [0,100,200,300,400,500,600,700,800,900,1000,1100,1200,1300,1400,1500,1600,1700,1800,1900,2000,2500]#,16000,32000,64000,120000,240000,480000,1000000,1000000*2,1000000*4,1000000*8,1000000*12]
-    xlab = ["0","100","200","300","400","500",'600','700','800','900',"1000",'1100','1200','1300','1400','1500','1600','1700','1800','1900',"2000",'2500']#,'16000','32000','64000','120000','240000',"480000",'1e06','2e06','4e06','8e06','16e06']
-    y = [0.1e-3,1e-3,1e-2,0.1,1.,10.,60.]
-    ylab = ["0.1 ms","1 ms","10 ms","0.1 s","1 s","10 s","1 min"] 
-
-    for name in names:
-        for i in range(len(Nlist)):
-            times_list = []
-            file = open(name,'r')
-            results_matrix = [[(num) for num in line.split(' ')] for line in file]
-            for i in range(len(Nlist)):
-                times_yi_values = results_matrix[i][1]
-                times_list.append(float(times_yi_values))
-
-        file.close()
-
+        for b in range(Nbandwidth):
+            files[iterator].write(f"{Bandwidth[b]} {dts_mean[i]}\n")
+            files[iterator+1].write(f"{Bandwidth[b]} {dtd_mean[i]}\n")
+            files[iterator].flush()
+            files[iterator+1].flush()
+    iterator +=2            
+    
 print("Files ready to be imported")
--- graficador.py
+++ graficador.py
@@ -1,6 +1,3 @@
-import numpy as np 
-from numpy import zeros,float32
-from scipy import linalg
 from time import perf_counter
 import matplotlib.pyplot as plt
 import os 
@@ -9,13 +6,10 @@ path = 'C:/Users/oioya/OneDrive/Escritorio/Cluster-CPU-Amd-Ryzen-9-5950-Benchmar
 list1 = [2, 5, 10,12, 15, 20,30, 40, 45, 50, 55,60, 75, 100,125, 160, 200,250, 350, 500,600, 800,1000,2000]
 names = os.listdir(path)
 
-#labels y escalados para el grafico
-x = [10,20,50,100,200,500,1000,2000,5000,10000,20000]
-xlab = ["10","20","50","100","200","500","1000","2000","5000","10000","20000"]
+xaxis = [10,20,50,100,200,500,1000,2000,5000,10000,20000]
+xlabel = ["10","20","50","100","200","500","1000","2000","5000","10000","20000"]
 y = [0.1e-3,1e-3,1e-2,0.1,1.,10.,60,60*10, 60*100]
-ylab = ["0.1 ms","1 ms","10 ms","0.1 s","1 s","10 s","1 min","10 min"] 
-y2 = [10**3,10**4,10**5,10**6,10**7,10**8,10**9,10**10]
-ylab2 = ["1 KB","10 KB","100 KB","1 MB","10 MB","100 MB","1 GB","10 GB","100 GB"]
+ylabel = ["0.1 ms","1 ms","10 ms","0.1 s","1 s","10 s","1 min","10 min"] 
 
 #valores que tomara y en la lista times_list
 for i in range(len(list1)):
